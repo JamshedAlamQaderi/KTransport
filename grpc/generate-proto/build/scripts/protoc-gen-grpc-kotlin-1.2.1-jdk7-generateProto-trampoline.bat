653b3ab9b1349c905597d5d30b6a111a3b85ad10
Jamshed Alam Qaderi <jamshedalamqaderi@gmail.com>
2023-01-09 12:07:34 +0600

    ignored build dir

--- grpc/generate-proto/build/scripts/protoc-gen-grpc-kotlin-1.2.1-jdk7-generateProto-trampoline.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files\Zulu\zulu-17\bin\java.exe" -jar "C:\Users\USER\.gradle\caches\modules-2\files-2.1\io.grpc\protoc-gen-grpc-kotlin\1.2.1\275f21cbd908b913fbccd35f497b6e35c4f9d0d1\protoc-gen-grpc-kotlin-1.2.1-jdk7.jar" %*
