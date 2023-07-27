1f17ded5ef80208c7f189c616bdc87c20cb4292f
Jamshed Alam Qaderi <jamshedalamqaderi@gmail.com>
2023-07-27 11:07:26 +0600

    added plugin and extension in gradle-plugin

---
+++ gradle-plugin/src/main/kotlin/com/jamshedalamqaderi/ktransport/plugin/KTransportDeps.kt
@@ -0,0 +1,7 @@
+package com.jamshedalamqaderi.ktransport.plugin
+
+object KTransportDeps {
+    private val version = "0.0.1-SNAPSHOT"
+    val ksp = "com.jamshedalamqaderi.ktransport:ktransport-ksp:$version"
+    val ktransport = "com.jamshedalamqaderi.ktransport:ktransport:$version"
+}
