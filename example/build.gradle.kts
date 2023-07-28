61f94b2d9ea0998ec1b534afff3c22ade24d4bd8
Jamshed Alam Qaderi <jamshedalamqaderi@gmail.com>
2023-07-28 17:45:56 +0600

    changed version to 1.0.1 for example module

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,21 +1,23 @@
+import com.jamshedalamqaderi.ktransport.plugin.KTransportDeps
+
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization")
-//    id("com.jamshedalamqaderi.ktransport.gradle-plugin") version "1.0.0"
-    id("com.google.devtools.ksp")
+    id("com.jamshedalamqaderi.ktransport.gradle-plugin") version "1.0.1"
+//    id("com.google.devtools.ksp")
 }
 
-//ktransport {
-//    packageName = "com.jamshedalamqaderi.ktransport.example.ksp"
+ktransport {
+    packageName = "com.jamshedalamqaderi.ktransport.example.ksp"
+}
+
+//ksp {
+//    arg(
+//        "buildDir",
+//        buildDir.absolutePath
+//    )
+//    arg("packageName", "com.jamshedalamqaderi.ktransport.example.ksp")
 //}
-
-ksp {
-    arg(
-        "buildDir",
-        buildDir.absolutePath
-    )
-    arg("packageName", "com.jamshedalamqaderi.ktransport.example.ksp")
-}
 
 val kotlinxSerializationVersion: String by project
 val coroutineVersion: String by project
@@ -30,8 +32,8 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(project(":ktransport"))
-//                implementation("com.jamshedalamqaderi.ktransport:ktransport:0.0.1-dev-18")
+//                implementation(project(":ktransport"))
+                implementation(KTransportDeps.ktransport)
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
             }
@@ -49,7 +51,7 @@ kotlin {
     }
 }
 
-dependencies {
-    add("kspJvm", project(":ktransport-ksp"))
-//    add("kspJvm", "com.jamshedalamqaderi.ktransport:ktransport-ksp:0.0.1-dev-18")
-}
+//dependencies {
+//    add("kspJvm", project(":ktransport-ksp"))
+////    add("kspJvm", "com.jamshedalamqaderi.ktransport:ktransport-ksp:0.0.1-dev-18")
+//}
